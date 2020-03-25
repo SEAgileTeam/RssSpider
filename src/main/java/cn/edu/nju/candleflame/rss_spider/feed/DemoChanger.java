a9e22a68104f8e96fd7f3623f3396fd715e390e2
weimin.li <weimin.li@transwarp.io>
2020-03-26 01:34:37 +0800

    [dev] change demo

--- src/main/java/cn/edu/nju/candleflame/rss_spider/feed/DemoChanger.java
+++ src/main/java/cn/edu/nju/candleflame/rss_spider/feed/DemoChanger.java
@@ -1,12 +1,24 @@
 package cn.edu.nju.candleflame.rss_spider.feed;
 
+import cn.edu.nju.candleflame.rss_spider.model.Item;
 import cn.edu.nju.candleflame.rss_spider.model.RssDocument;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class DemoChanger implements FeedChanger {
 	@Override
 	public RssDocument analysis(String html) {
-		return new RssDocument("title","url", "description");
+		Item item =  new Item("title","link","content");
+		List<Item> itemList = Arrays.asList(new Item("title1","link1","content1"),
+				new Item("title2","link2","content2"));
+
+		RssDocument rssDocument = new RssDocument("title", "url", "description");
+		rssDocument.appendItem(item);
+		rssDocument.appendAllItems(itemList);
+
+		return rssDocument;
 	}
 }
